19039775cbd169d46b9502595dc635b5e7e407f9
Figaarillo <axel.leonardi.22@gmail.com>
2023-06-09 19:20:00 -0300

    feat: add husky and git hook as lint-staged and commitlint

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,7 +4,7 @@ class UserController {
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   getUsers(req: Request, res: Response) {
     res.status(201).json({
-      message: 'Hello, user!'
+      message: 'Hello, user!',
     })
   }
 }
