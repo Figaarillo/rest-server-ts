4c7fca72a28521b98450ade7718c57843aedc02a
Figaarillo <axel.leonardi.22@gmail.com>
2023-07-19 11:32:47 -0300

    feat: correct name 'customer' to 'category' in category entity

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, OneToOne } from 'typeorm'
 import BaseEntity from '../../config/base.entity'
 import ProductEntity from '../../product/entities/product.entity'
 
-@Entity({ name: 'customer' })
+@Entity({ name: 'category' })
 class CategoryEntity extends BaseEntity {
   @Column()
   name!: string
