f4c8af0207724edc3824ecbd3808bf3be32e6bb1
Figaarillo <axel.leonardi.22@gmail.com>
2023-09-21 11:13:18 -0300

    feat: add handler errors

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,66 +1,83 @@
 import { type Request, type Response } from 'express'
 import UserService from '../services/user.service'
+import { HttpResponse } from '../../shared/response/http.response'
 
 class UserController {
   private readonly userService: UserService
+  private readonly httpResponse: HttpResponse
 
   constructor() {
     this.userService = new UserService()
+    this.httpResponse = new HttpResponse()
   }
 
-  async createUser(req: Request, res: Response): Promise<void> {
+  async createUser(req: Request, res: Response): Promise<Response> {
     try {
       const data = await this.userService.create(req.body)
 
-      res.status(201).json(data)
+      return this.httpResponse.Ok(res, data)
     } catch (error) {
-      console.error(error)
+      return this.httpResponse.Ok(res, error)
     }
   }
 
-  async deleteUser(req: Request, res: Response): Promise<void> {
+  async deleteUser(req: Request, res: Response): Promise<Response> {
     const { id } = req.params
 
     try {
       const data = await this.userService.delete(id)
 
-      res.status(200).json(data)
+      if (data.affected === 0) {
+        return this.httpResponse.NotFound(res, 'Error updating user')
+      }
+
+      return this.httpResponse.Ok(res, data)
     } catch (error) {
-      console.error(error)
+      return this.httpResponse.Ok(res, error)
     }
   }
 
-  async getUsers(_req: Request, res: Response): Promise<void> {
+  async getUsers(_req: Request, res: Response): Promise<Response | undefined> {
     try {
       const data = await this.userService.getAll()
+      if (data.length === 0) {
+        return this.httpResponse.NotFound(res, 'User not found')
+      }
 
-      res.status(200).json(data)
+      return this.httpResponse.Ok(res, data)
     } catch (error) {
-      console.error(error)
+      return this.httpResponse.Ok(res, error)
     }
   }
 
-  async getUserById(req: Request, res: Response): Promise<void> {
+  async getUserById(req: Request, res: Response): Promise<Response> {
     const { id } = req.params
 
     try {
       const data = await this.userService.getById(id)
+      if (data == null) {
+        return this.httpResponse.NotFound(res, 'User not found')
+      }
 
-      res.status(200).json(data)
+      return this.httpResponse.Ok(res, data)
     } catch (error) {
-      console.error(error)
+      return this.httpResponse.Ok(res, error)
     }
   }
 
-  async updateUser(req: Request, res: Response): Promise<void> {
+  async updateUser(req: Request, res: Response): Promise<Response> {
     const { id } = req.params
 
     try {
       const data = await this.userService.update(id, req.body)
 
-      res.status(200).json(data)
+      if (data.affected === 0) {
+        return this.httpResponse.NotFound(res, 'Error updating user')
+      }
+
+      return this.httpResponse.Ok(res, data)
     } catch (error) {
-      console.error(error)
+      return this.httpResponse.Ok(res, error)
     }
   }
 }
