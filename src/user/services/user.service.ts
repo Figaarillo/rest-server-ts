7142f0fb0f7d81126101e32b1be81f7bad4489db
Figaarillo <axel.leonardi.22@gmail.com>
2023-09-24 02:27:05 -0300

    feat: add password encryptor and new user services

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -2,6 +2,7 @@ import { type UpdateResult, type DeleteResult } from 'typeorm'
 import { BaseService } from '../../config/base.service'
 import UserEntity from '../entities/user.entity'
 import type UserDTO from '../dtos/user.dto'
+import bcrypt from 'bcrypt'
 
 class UserService extends BaseService<UserEntity> {
   constructor() {
@@ -9,7 +10,13 @@ class UserService extends BaseService<UserEntity> {
   }
 
   async create(user: UserEntity): Promise<UserEntity> {
-    return await (await this.execRepository).save(user)
+    const newUser = await (await this.execRepository).save(user)
+
+    const hashedPassword = await bcrypt.hash(newUser.password, 10)
+
+    newUser.password = hashedPassword
+
+    return await (await this.execRepository).save(newUser)
   }
 
   async delete(id: string): Promise<DeleteResult> {
@@ -22,6 +29,22 @@ class UserService extends BaseService<UserEntity> {
 
   async getById(id: string): Promise<UserEntity | null> {
     return await (await this.execRepository).findOneBy({ id })
+  }
+
+  async getByEmail(email: string): Promise<UserEntity | null> {
+    return await (await this.execRepository)
+      .createQueryBuilder('user')
+      .addSelect('user.password')
+      .where({ email })
+      .getOne()
+  }
+
+  async getByUsername(username: string): Promise<UserEntity | null> {
+    return await (await this.execRepository)
+      .createQueryBuilder('user')
+      .addSelect('user.password')
+      .where({ username })
+      .getOne()
   }
 
   async update(id: string, infoUpdate: UserDTO): Promise<UpdateResult> {
